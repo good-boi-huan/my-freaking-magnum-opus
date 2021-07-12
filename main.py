af0bfeed622350d14bc0e5cf851809d192fedbd7
hoobab <87278869+good-boi-huan@users.noreply.github.com>
2021-07-12 23:06:00 +0000

    data conversion, arithmetic operators, get perimeter of rectangle

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-# Print Hello Arda
-print("hello")
+# # Print Hello Arda
+# print("hello")
 
-name = input("What is your name? ")
-age = input("How old are you? ")
-fav_activity = input("What is your favorite activity? ")
-book = input("What is your favorite book? ")
+# name = input("What is your name? ")
+# age = input("How old are you? ")
+# fav_activity = input("What is your favorite activity? ")
+# book = input("What is your favorite book? ")
 
-# Concatination
-print("Your name is " + name)
-print("Your age is " + age)
-print("You favorite activity is " + fav_activity)
-print("Your favorite book is " + book)
+# # Concatination
+# print("Your name is " + name)
+# print("Your age is " + age)
+# print("You favorite activity is " + fav_activity)
+# print("Your favorite book is " + book)
+
+# # Data Type Conversion
+# var_1 = 1
+# var_string = str(var_1)
+# print(var_1)
+# print(var_string)
+# # int()
+# # float()
+# # str()
+
+# Arithmetic Operators
+# addition + 
+# subtraction - 
+# multiplication *
+# division /
+
+# Get perimeter of rectangle
+# User inputs length and width
+# Console outputs perimeter
+# length = input("Give me a length ")
+# width = input("Give me a width ")
+# length2 = float(length)
+# width2 = float(width)
+# perimeter = (2.0*length2)+(2.0*width2)
+
+# print("The perimeter of a",length,"by",width,"rectangle is",perimeter)
+
+# String manip
+# Concatination: "hello" + "world" = "hello world"
+# length
+# upper
+# lower
