fdfff8328af98f1d9a840d86d84a9f43579838c3
hoobab <87278869+good-boi-huan@users.noreply.github.com>
2021-07-12 22:34:31 +0000

    different data types

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+# Print Hello Arda
 print("hello arda")
+
+# Print Age
 age = input("how old are you ")
 print(age)
+
+# Data types
+# string - characters/words (characters: a.b,c,d string: hello)
+# float/double - decimals (42.2323)
+# integer - whole numbers (244)
+# boolean - true/false
+
+var_1 = "hello"
+var_2 = 123892692
+var_3 = 2389.325325
+var_4 = True
+
+print(var_1)
+print(var_2)
+print(var_1)
+print(var_1)
+print(var_1)
