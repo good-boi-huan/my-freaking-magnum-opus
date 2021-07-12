d7690756748e133eee77f492718df7931cfeef86
hoobab <87278869+good-boi-huan@users.noreply.github.com>
2021-07-12 22:42:06 +0000

    concatination

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 # Print Hello Arda
-print("hello arda")
+print("hello")
 
-# Print Age
-age = input("how old are you ")
-print(age)
+name = input("What is your name? ")
+age = input("How old are you? ")
+fav_activity = input("What is your favorite activity? ")
+book = input("What is your favorite book? ")
 
-# Data types
-# string - characters/words (characters: a.b,c,d string: hello)
-# float/double - decimals (42.2323)
-# integer - whole numbers (244)
-# boolean - true/false
-
-var_1 = "hello"
-var_2 = 123892692
-var_3 = 2389.325325
-var_4 = True
-
-print(var_1)
-print(var_2)
-print(var_1)
-print(var_1)
-print(var_1)
+# Concatination
+print("Your name is " + name)
+print("Your age is " + age)
+print("You favorite activity is " + fav_activity)
+print("Your favorite book is " + book)
